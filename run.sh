0417a04be5e82c52ea9ad6f32c650e0d8f0ff2d5
arthur <arthur.karganyan@gmail.com>
2017-09-02 21:32:01 +0300

    a

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-rvm use $(echo $(ruby -e 'print RUBY_VERSION'))@${PWD##*/} do ruby main.rb $(zenity --file-selection --directory)
+#!/bin/bash
+
+/home/$USER/.rvm/bin/rvm use $(echo $(ruby -e 'print RUBY_VERSION'))@${PWD##*/} do ruby main.rb $(zenity --info --text "Выберите папку с .docx оригиналами" && zenity --file-selection --directory)
